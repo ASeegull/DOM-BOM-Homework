24e3b3f778c5fcbe15cea25c4554897a14395a11
Anna <a.chaikalemberg@gmail.com>
2017-06-02 01:06:01 +0300

    Binds creation of signup form to the button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
+function createPopup() {
     var createContainer = document.createElement('div');
-        createContainer.setAttribute('class','form-container');
+    createContainer.setAttribute('class', 'form-container');
     document.body.appendChild(createContainer);
     var container = document.querySelector('div.form-container');
     var createHeading = document.createElement('h2');
@@ -16,7 +16,8 @@
     var joined_lb = document.createElement('label');
     var joined_in = document.createElement('input');
     var submit = document.createElement('input');
-container.appendChild(form);
+
+    container.appendChild(form);
     var formAppend = [username_lb, username_in, nickname_lb, nickname_in, age_lb, age_in, joined_lb, joined_in, submit];
 
     function setAttributes(el, attrs) {
@@ -27,47 +28,47 @@ container.appendChild(form);
 
     setAttributes(form, {
         "id": "user-login",
-        "name":  "login",
-    "action": "https://aseegull.github.io/DOM-BOM-Homework",
-    "method": "post",
-    "autocomplete":"off",
-    "onsubmit": "return validateForm();"
+        "name": "login",
+        "action": "https://aseegull.github.io/DOM-BOM-Homework",
+        // "method": "get",
+        "autocomplete": "off",
+        "onsubmit": "setAvatar();"
     });
 
     setAttributes(username_in, {
         "class": "pass",
         "id": "username",
-        "name":  "username",
+        "name": "username",
         "type": "text",
         "placeholder": 'like "anyuser_25kld"',
-        "autocomplete":"off"
+        "autocomplete": "off"
     });
 
     setAttributes(nickname_in, {
         "class": "pass",
         "id": "nickname",
-        "name":  "nickname",
+        "name": "nickname",
         "type": "text",
         "placeholder": 'emma.watson',
-        "autocomplete":"off"
+        "autocomplete": "off"
     });
 
     setAttributes(age_in, {
         "class": "pass",
         "id": "age",
-        "name":  "nickname",
+        "name": "nickname",
         "type": "text",
         "placeholder": '18',
-        "autocomplete":"off"
+        "autocomplete": "off"
     });
 
     setAttributes(joined_in, {
         "class": "pass",
         "id": "joined",
-        "name":  "date",
+        "name": "date",
         "type": "text",
         "placeholder": '24/04/2017',
-        "autocomplete":"off"
+        "autocomplete": "off"
     });
 
     setAttributes(submit, {
@@ -75,7 +76,7 @@ container.appendChild(form);
         "id": "submit",
         "type": "submit",
         "value": "Register",
-        "disabled":"true"
+        "disabled": "true"
     });
 
     nickname_lb.innerHTML = "Enter your nickname:<br /><span class='small'>" + "(It can contain numbers but it's better<br />" +
@@ -90,97 +91,113 @@ container.appendChild(form);
 
     var getForm = document.getElementById('user-login');
 
-    formAppend.forEach(function(el) {
-    getForm.appendChild(el);
+    formAppend.forEach(function (el) {
+        getForm.appendChild(el);
     });
+    document.getElementById('signUp').disabled = true;
+    formValidation();
+}
 
-    function ageValidation(age_in) {
-        var val = age_in.value;
-        var validate = ( isNaN(val) || val <= 0 || val === '');
-        if (!validate) {
-            this.setAttribute('class', 'invalidInput');
-            alert('Your data is invalid');
-        }
-        else {
-            this.setAttribute('class', 'pass');
-            this.setAttribute('data-valid', 'valid');
-        }
-    }
 
+function formValidation() {
     age_in.addEventListener('blur', ageValidation);
-
-    function usernameValidation(username_in) {
-        var val = username_in.value;
-        var usernameReg = /user_[a-zA-Z0-9*&^$#@]* /;
-        var validate = usernameReg.test(val);
-        if (!validate) {
-            this.setAttribute('class', 'invalidInput');
-            alert('Your data is invalid');
-        }
-        else {
-            this.setAttribute('class', 'pass');
-            this.setAttribute('data-valid', 'valid');
-            submitEnable();
-        }
-    }
     username_in.addEventListener('blur', usernameValidation, false);
-
-
-    function dateValidation(joined_in) {
-        var val = joined_in.value;
-        var dateReg = / [0-9]{2}\/[0-9]{2}\/[0-9]{4} /;
-        var validate = dateReg.test(val);
-        if (!validate) {
-            this.setAttribute('class', 'invalidInput');
-            alert('Your data is invalid');
-        }
-        else {
-            this.setAttribute('class', 'pass');
-            this.setAttribute('data-valid', 'valid');
-            submitEnable();
-        }
-    }
     joined_in.addEventListener('blur', dateValidation, false);
-
-    function nicknameValidation(nickname_in) {
-        var val = nickname_in.value;
-        var nickReg = / [0-9a-zA-Z]+\.[0-9a-zA-Z]+ /;
-        var validate = nickReg.test(val);
-        if (!validate) {
-            this.setAttribute('class', 'invalidInput');
-            alert('Your data is invalid');
-        }
-        else {
-            this.setAttribute('class', 'pass');
-            this.setAttribute('data-valid', 'valid');
-            submitEnable();
-        }
-    }
-
     nickname_in.addEventListener('blur', nicknameValidation, false);
+    document.addEventListener('keyup', submitEnable, false);
+}
 
-    function submitEnable() {
-        var validation = getForm.querySelectorAll('[data-valid]');
-        if (validation.length === 4) {
-            submit.disabled = false;
-        }
+
+
+
+
+function ageValidation(age_in) {
+    var val = age_in.value;
+    var validate = ( isNaN(val) || val <= 0 || val === '');
+    if (!validate) {
+        this.setAttribute('class', 'invalidInput');
+        alert('Your data is invalid');
+    }
+    else {
+        this.setAttribute('class', 'pass');
+        this.setAttribute('data-valid', 'valid');
+    }
+}
+
+
+function usernameValidation(username_in) {
+    var val = username_in.value;
+    var usernameReg = /user_[a-zA-Z0-9*&^$#@]* /;
+    var validate = usernameReg.test(val);
+    if (!validate) {
+        this.setAttribute('class', 'invalidInput');
+        alert('Your data is invalid');
+    }
+    else {
+        this.setAttribute('class', 'pass');
+        this.setAttribute('data-valid', 'valid');
+    }
+}
+
+
+
+function dateValidation(joined_in) {
+    var val = joined_in.value;
+    var dateReg = / [0-9]{2}\/[0-9]{2}\/[0-9]{4} /;
+    var validate = dateReg.test(val);
+    if (!validate) {
+        this.setAttribute('class', 'invalidInput');
+        alert('Your data is invalid');
+    }
+    else {
+        this.setAttribute('class', 'pass');
+        this.setAttribute('data-valid', 'valid');
+    }
+}
+
+
+function nicknameValidation(nickname_in) {
+    var val = nickname_in.value;
+    var nickReg = / [0-9a-zA-Z]+\.[0-9a-zA-Z]+ /;
+    var validate = nickReg.test(val);
+    if (!validate) {
+        this.setAttribute('class', 'invalidInput');
+        alert('Your data is invalid');
+    }
+    else {
+        this.setAttribute('class', 'pass');
+        this.setAttribute('data-valid', 'valid');
+    }
+}
+
+
+
+function submitEnable() {
+    var validation = getForm.querySelectorAll('[data-valid]');
+    if (validation.length === 4) {
+        submit.disabled = false;
+    }
+}
+
+
+
+window.addEventListener('scroll', function () {
+    var cords = document.querySelectorAll('.cord');
+    var parallax = document.getElementById('parallax-container');
+    var prl = document.getElementById('parallax');
+    var posY = document.body.scrollTop;
+    Array.from(document.getElementsByClassName('myClass')).forEach(function (element) {
+        console.log(element.id);
+    });
+
+    parallax.style.backgroundPosition = '0px ' + -posY * 0.8 + 'px';
+    prl.style.backgroundPosition = '0px ' + (200 + -posY * 0.72) + 'px';
+
+    function scrollCords(item) {
+        item.style.backgroundPosition = '0px ' + -posY * 10 + 'px';
     }
 
-    window.addEventListener('scroll', function () {
-        console.log(document.body.scrollTop);
-        var cords = document.querySelectorAll('.cord');
-        var parallax = document.getElementById('parallax-container');
-        var prl = document.getElementById('parallax');
-        var posY = document.body.scrollTop * 0.4;
-        Array.from(document.getElementsByClassName('myClass')).forEach(function(element) {
-            console.log(element.id);
-        });
+    cords.forEach(scrollCords);
+});
 
-            parallax.style.backgroundPosition = '0px ' + -posY + 'px';
-        prl.style.backgroundPosition = '0px ' + -posY + 'px';
-
-        function scrollCords(item) {
-            item.style.backgroundPosition = '0px ' + -posY * 5 + 'px';
-        }
-        cords.forEach(scrollCords);
-    });
+// TODO setAvatar();
