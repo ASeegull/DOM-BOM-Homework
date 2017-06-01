4d29eb6dcd94e1c8db41e1990ffd39dc1794f35b
Anna <a.chaikalemberg@gmail.com>
2017-06-01 23:13:33 +0300

    Adds paralax

--- script.js
+++ script.js
@@ -15,8 +15,9 @@
     var age_in = document.createElement('input');
     var joined_lb = document.createElement('label');
     var joined_in = document.createElement('input');
+    var submit = document.createElement('input');
 container.appendChild(form);
-    var formAppend = [username_lb, username_in, nickname_lb, nickname_in, age_lb, age_in, joined_lb, joined_in];
+    var formAppend = [username_lb, username_in, nickname_lb, nickname_in, age_lb, age_in, joined_lb, joined_in, submit];
 
     function setAttributes(el, attrs) {
         for (var key in attrs) {
@@ -55,22 +56,30 @@ container.appendChild(form);
         "class": "pass",
         "id": "age",
         "name":  "nickname",
-        "type": "number",
+        "type": "text",
         "placeholder": '18',
         "autocomplete":"off"
     });
 
     setAttributes(joined_in, {
         "class": "pass",
-        "id": "age",
-        "name":  "nickname",
+        "id": "joined",
+        "name":  "date",
+        "type": "text",
+        "placeholder": '24/04/2017',
+        "autocomplete":"off"
+    });
+
+    setAttributes(submit, {
+        "class": "pass",
+        "id": "submit",
         "type": "submit",
         "value": "Register",
         "disabled":"true"
     });
 
-    nickname_lb.innerHTML = "Enter your nickname:<br /><span class='small'>" + "(It could be whatever you like<br />" +
-        "but we recommend to use something like this</span>";
+    nickname_lb.innerHTML = "Enter your nickname:<br /><span class='small'>" + "(It can contain numbers but it's better<br />" +
+        "to use just letters with one dot something like this</span>";
     nickname_lb.setAttribute('for', 'nickname');
     username_lb.innerHTML = "Enter your username<br />" + "It must contain <kbd>user_</kbd>";
     username_lb.setAttribute('for', 'username');
@@ -80,8 +89,98 @@ container.appendChild(form);
     joined_lb.setAttribute('for', 'joined');
 
     var getForm = document.getElementById('user-login');
-    getForm.appendChild(nickname_in);
 
     formAppend.forEach(function(el) {
     getForm.appendChild(el);
+    });
+
+    function ageValidation(age_in) {
+        var val = age_in.value;
+        var validate = ( isNaN(val) || val <= 0 || val === '');
+        if (!validate) {
+            this.setAttribute('class', 'invalidInput');
+            alert('Your data is invalid');
+        }
+        else {
+            this.setAttribute('class', 'pass');
+            this.setAttribute('data-valid', 'valid');
+        }
+    }
+
+    age_in.addEventListener('blur', ageValidation);
+
+    function usernameValidation(username_in) {
+        var val = username_in.value;
+        var usernameReg = /user_[a-zA-Z0-9*&^$#@]* /;
+        var validate = usernameReg.test(val);
+        if (!validate) {
+            this.setAttribute('class', 'invalidInput');
+            alert('Your data is invalid');
+        }
+        else {
+            this.setAttribute('class', 'pass');
+            this.setAttribute('data-valid', 'valid');
+            submitEnable();
+        }
+    }
+    username_in.addEventListener('blur', usernameValidation, false);
+
+
+    function dateValidation(joined_in) {
+        var val = joined_in.value;
+        var dateReg = / [0-9]{2}\/[0-9]{2}\/[0-9]{4} /;
+        var validate = dateReg.test(val);
+        if (!validate) {
+            this.setAttribute('class', 'invalidInput');
+            alert('Your data is invalid');
+        }
+        else {
+            this.setAttribute('class', 'pass');
+            this.setAttribute('data-valid', 'valid');
+            submitEnable();
+        }
+    }
+    joined_in.addEventListener('blur', dateValidation, false);
+
+    function nicknameValidation(nickname_in) {
+        var val = nickname_in.value;
+        var nickReg = / [0-9a-zA-Z]+\.[0-9a-zA-Z]+ /;
+        var validate = nickReg.test(val);
+        if (!validate) {
+            this.setAttribute('class', 'invalidInput');
+            alert('Your data is invalid');
+        }
+        else {
+            this.setAttribute('class', 'pass');
+            this.setAttribute('data-valid', 'valid');
+            submitEnable();
+        }
+    }
+
+    nickname_in.addEventListener('blur', nicknameValidation, false);
+
+    function submitEnable() {
+        var validation = getForm.querySelectorAll('[data-valid]');
+        if (validation.length === 4) {
+            submit.disabled = false;
+        }
+    }
+
+    window.addEventListener('scroll', function () {
+        console.log(document.body.scrollTop);
+        var cords = document.querySelectorAll('.cord');
+        var parallax = document.getElementById('parallax-container');
+        var prl = document.getElementById('parallax');
+        var posY = document.body.scrollTop * 0.4;
+        Array.from(document.getElementsByClassName('myClass')).forEach(function(element) {
+            console.log(element.id);
+        });
+
+            parallax.style.backgroundPosition = '0px ' + -posY + 'px';
+        prl.style.backgroundPosition = '0px ' + -posY + 'px';
+
+        function scrollCords(item) {
+            item.style.backgroundPosition = '0px ' + -posY * 5 + 'px';
+        }
+        cords.forEach(scrollCords);
     });
