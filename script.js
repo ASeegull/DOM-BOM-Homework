c3f95cc07be72fa060d7bf0d9b8c7cdfce85f860
Anna <a.chaikalemberg@gmail.com>
2017-06-09 23:09:34 +0300

    Fixes form validation and localization

--- script.js
+++ script.js
@@ -1,129 +1,88 @@
-window.addEventListener('scroll', function () {
-    var cords = document.querySelectorAll('.cord');
-    var parallax = document.getElementById('parallax-container');
-    var prl = document.getElementById('parallax');
-    var posY = document.body.scrollTop;
+//Task OS message
+
+function setOSMessageInnerText() {
+    var agent = navigator.userAgent.toLowerCase(),
+        message = document.getElementById('message');
+
+    if ((agent.indexOf('win') !== -1)) {
+        message.innerText = 'Windows';				
+    } else if (agent.indexOf('lin') !== -1) {
+        message.innerText = 'Linux';
+    } else if (agent.indexOf('mac') !== -1) {
+        message.innerText = 'MacOS';
+    } else {
+        message.innerText = 'OS system';
+    }
+}
+
+
+// Parallax
+
+function parallaxScroll() {
+    var cords = document.querySelectorAll('.cord'),
+        parallax = document.getElementById('parallax-container'),
+        prl = document.getElementById('parallax'),
+        posY = document.body.scrollTop;
+
     parallax.style.backgroundPosition = '0px ' + -posY * 0.8 + 'px';
     prl.style.backgroundPosition = '0px ' + (220 + -posY * 0.72) + 'px';
 
-    function scrollCords(item) {
+    cords.forEach(function(item) {
         item.style.backgroundPosition = '0px ' + -posY * 10 + 'px';
-    }
-
-    cords.forEach(scrollCords);
-});
-
-document.getElementById("signUp").onclick = function () {
-    location.href = "form.html";
-};
-
-// TODO setAvatar();
-
-//document.querySelector('.'+storedLang);  ----------- to select span
-
-//function setLanguage() {
-//    var radioButtons = document.querySelectorAll('input[name="lang"]');
-//    var getLanguage = function(radioButtons) {
-//        for (var i = 0; i <= radioButtons.length; i++) {
-//            if (item.checked) {
-//           var lang = item.getAttribute('data-lang');
-//            if (localStorage) {
-//                localStorage.setItem("data-lang", lang);
-//            }
-//        }
-//        }
-//    }
-//    }
-//
-//setLanguage();
-//
-//var langSelector = document.getElementById('languages');
-//langSelector.addEventListener('change', setLanguage, false);
-//
-//document.addEventListener('load', changeLanguage, false);
-//
-
-var agent = navigator.userAgent.toLowerCase();
-var message = document.getElementById('message');
-if ((agent.indexOf('win') !== -1)) {
-    message.innerText = 'Windows';
-} else if (agent.indexOf('lin') !== -1) {
-    message.innerText = 'Linux';
-} else if (agent.indexOf('mac') !== -1) {
-    message.innerText = 'MacOS';
-} else {
-    message.innerText = 'OS system';
+    });
 }
 
-// switch(agent) {
-//     case (agent.indexOf('win') !== -1 && agent.indexOf('64')):
-//         message.innerText = 'Windows 64-bit';
-//         break;
-//     case (agent.indexOf('win') !== -1 && agent.indexOf('32') !== -1):
-//         message.innerText = 'Windows 32-bit';
-//         break;
-//     case (agent.indexOf('lin') !== -1):
-//         message.innerText = 'Linux';
-//         break;
-//     case (agent.indexOf('mac') !== -1):
-//         message.innerText = 'MacOS';
-//         break;
-//     default:
-//         message.innerText = 'OS system'
-// }
+// Localization
 
-//
-//function checkLanguage() {    
-//    if (localStorage) {
-//        var storedLang = localStorage.getItem('data-lang');
-//        var lang = document.getElementById(storedLang);
-//        lang.checked = true;
-//        return;
-//    }
-//}
-//
-var radioButtons = document.querySelectorAll('input[name="lang"]');
-var localLang = localStorage.language;
+function getSavedLanguage() {
+    var localStorage = this.localStorage,
+        lang = 'lang-ua';
 
-
-function getLang() {
-  if (localLang) { //якщо є збережена мова
-    var radioButtons = document.querySelectorAll('input[name="lang"]');
-    var localLang = localStorage.language;
-    var localLang = localStorage.language;
-    for (var i = 0; i <= radioButtons.length; i++) { //пройдися по всіх кнопках 
-      if (radioButtons[i].value === localLang) { //якщо айдішка така ж як мова,
-        radioButtons[i].setAttribute('checked', 'checked'); //відмічає її
-      } else {
-        localStorage.setItem('language', 'lang-ua'); //Якщо мови нема у стореджі - задає дефолтну українську мову
-      }
-      changeLanguage(localLang); //запускає зміну мови
+    if (localStorage.language) {
+        lang = localStorage.language;
+        document.querySelector('#languages [value="' + lang + '"]').checked = true;
+    } else {
+        localStorage.setItem('language', lang);
     }
-  }
+
+    changeLanguage(lang);
 }
-document.addEventListener("DOMContentLoaded", getLang);
-document.getElementById('languages').addEventListener('change', setLanguage, true); //Запускає функції при зміні опції
+
 
 function setLanguage() {
-  for (var i = 0; i <= radioButtons.length; i++) {
-    var radioButtons = document.querySelectorAll('input[name="lang"]');
-    var localLang = localStorage.language;
-    if (radioButtons[i].checked) { // шукає чекнуту опцію
-      var lang = radioButtons[i].value; //Бере її атрибут з дата-ленг ('lang-ua') наприклад
-      localStorage.setItem("language", lang); //Зберігає мову у локал стореджі 
-      changeLanguage(localLang); //Запускає функцію зміни мови
-    }
-  }
+    var lang = document.querySelector('[name="lang"]:checked').value;
+
+    localStorage.setItem("language", lang);
+    changeLanguage(lang);
 }
 
-
-function changeLanguage(localLang) {
-  var langSpan = document.getElementsByClassName('lang'); //обирає усі спани з перекладами
-  for (var i = 0; i < langSpan.length; i++) { //прозодиться по кожному
-    if (langSpan[i].value === localLang) { //звіряє з айдішкою
-      langSpan[i].setAttribute('display', 'initial'); //та відновлює відображення за деолтом, якщо мова пасує
-    } else {
-      langSpan[i].setAttribute('display', 'none'); //ховає, якщо не підходить
+function changeLanguage(lang) {
+    var localizedContentEls = document.getElementsByClassName('lang'),
+        localizedContent,
+        i;
+	
+    for (i = 0; i < localizedContentEls.length; i++) {
+        localizedContent = localizedContentEls[i];
+        localizedContent.style.display = localizedContent.className.indexOf(lang) > -1 ? 'inline-block' : 'none';
     }
-  }
 }
+
+//Додає всі event listeners
+
+function addLanguageChangeListeners() {
+    document.getElementsByName('lang').forEach(function(item) {
+        item.addEventListener('change', setLanguage, false);
+    });
+}
+
+function addAllEventListeners() {
+    window.addEventListener('scroll', parallaxScroll);
+    window.addEventListener("DOMContentLoaded", getSavedLanguage);
+    document.getElementById("signUp").onclick = function() {
+        location.href = "form.html";
+    };
+    addLanguageChangeListeners();
+}
+
+setOSMessageInnerText();
+addAllEventListeners();
--- submit.js
+++ submit.js
@@ -1,23 +1,27 @@
-
 var createContainer = document.createElement('div');
 createContainer.setAttribute('class', 'form-container center');
-document.body.appendChild(createContainer);
-var container = document.querySelector('div.form-container');
-var createHeading = document.createElement('h2');
+document.body.appendChild(createContainer); //Створює контейнер для форми
+
+
+var container = document.querySelector('div.form-container'),
+	createHeading = document.createElement('h2');
 createHeading.innerText = 'Create your account';
-container.appendChild(createHeading);
-var form = document.createElement('form');
-var username_lb = document.createElement('label');
-var username_in = document.createElement('input');
-var nickname_lb = document.createElement('label');
-var nickname_in = document.createElement('input');
-var age_lb = document.createElement('label');
-var age_in = document.createElement('input');
-var joined_lb = document.createElement('label');
-var joined_in = document.createElement('input');
-var submit = document.createElement('input');
+container.appendChild(createHeading); //Створює та додає заголовок
+
+
+var form = document.createElement('form'),					// Створює
+	username_lb = document.createElement('label'),
+	username_in = document.createElement('input'),
+	nickname_lb = document.createElement('label'),			// поля вводу та підписи
+	nickname_in = document.createElement('input'),
+	age_lb = document.createElement('label'),
+	age_in = document.createElement('input'),
+	joined_lb = document.createElement('label'),			// жах просто
+	joined_in = document.createElement('input'),
+	submit = document.createElement('input'); 				// треба було принаймні вкладати input в label. І innerHTML юзати
 
 container.appendChild(form);
+	
 var formAppend = [username_lb, username_in, nickname_lb, nickname_in, age_lb, age_in, joined_lb, joined_in, submit];
 
 function setAttributes(el, attrs) {
@@ -30,7 +34,6 @@ setAttributes(form, {
     "id": "user-login",
     "name": "login",
     "action": "https://aseegull.github.io/DOM-BOM-Homework",
-    // "method": "get",
     "autocomplete": "off",
     "onsubmit": "setAvatar();"
 });
@@ -40,7 +43,7 @@ setAttributes(username_in, {
     "id": "username",
     "name": "username",
     "type": "text",
-    "placeholder": 'like "anyuser_25kld"',
+    "placeholder": 'like "user_25kld"',
     "autocomplete": "off"
 });
 
@@ -78,11 +81,14 @@ setAttributes(submit, {
     "value": "Register",
     "disabled": "true"
 });
+
+
+//Додає підписи до полів
 var getForm = document.getElementById('user-login');
 nickname_lb.innerHTML = "Enter your nickname:<br /><span class='small'>" + "(It can contain numbers but it's better<br />" +
     "to use just letters with one dot something like this</span>";
 nickname_lb.setAttribute('for', 'nickname');
-username_lb.innerHTML = "Enter your username<br />" + "It must contain <kbd>user_</kbd>";
+username_lb.innerHTML = "Enter your username<br />" + "It must start with <kbd>user_</kbd>";
 username_lb.setAttribute('for', 'username');
 age_lb.innerText = "How old are you?";
 age_lb.setAttribute('for', 'age');
@@ -98,77 +104,66 @@ formAppend.forEach(function (el) {
 getForm.addEventListener('click', initValidation, false);
 
 function initValidation() {
+//	var
     age_in.addEventListener('blur', ageValidation, false);
-    // age_in.addEventListener('keyup', ageValidation, false);
     username_in.addEventListener('blur', usernameValidation, false);
     joined_in.addEventListener('blur', dateValidation, false);
     nickname_in.addEventListener('blur', nicknameValidation, false);
 }
 
 
-
-
-function ageValidation(age_in) {
-    var validate = (isNaN(+age_in) || (+age_in) <= 0); //Повинно ж давати True якщо дані невірні, не чисельні
-    if (!validate) {
-        this.setAttribute('class', 'invalidInput'); //Тому має виконуватися оцей блок
-        alert('Your data is invalid');              //Але він виконується ЗАВЖДИ
-    }
-    else {
-        this.setAttribute('data-valid', 'valid'); //Коли ж все добре, мали б додаватися
-        // додаються дані, які потрібні для валідаіції
+function checkInput(validate, input) {
+	if (!validate) {
+        input.setAttribute('class', 'invalidInput');
+        alert('Your data is invalid');
+    } else {
+        input.setAttribute('class', 'pass');
+        input.setAttribute('data-valid', 'valid');
         submitEnable();
     }
 }
 
-
-function usernameValidation(username_in) {
-    var usernameReg = /^user_[a-zA-Z0-9;:#*%S]*/;//Пробувала оналайн-валідатори регулярних виразів, наче підходило
-    var validate = String(username_in).match(usernameReg); //Я пробувала і варіант з методом test...
-    // без String консолька видає помилку
-
-    if (validate === -1) {
-        this.setAttribute('class', 'invalidInput');
-        alert('Your data is invalid');
-    }
-    else {
-        this.setAttribute('class', 'pass');
-        this.setAttribute('data-valid', 'valid');
-        submitEnable();
-    }
+function ageValidation() {
+	var age = document.getElementById('age');
+    
+	if (isNaN(age.value)) {
+		age.setAttribute('class', 'invalidInput');
+		(function () { alert('Please, enter number')})();
+	} else if (age.value <= 0) {
+		age.setAttribute('class', 'invalidInput');
+		(function () { alert('Age can\'t be zero or less, can it?')} )(); 
+	} else {
+		age.setAttribute('class', 'pass');
+		age.setAttribute('data-valid', 'valid');
+		submitEnable();
+	}
 }
 
 
-
-function dateValidation(joined_in) {
-    // var val = joined_in.value;
-    var dateReg = / ^[0-9]{2}\/[0-9]{2}\/[0-9]{4} /;
-    var validate = String(joined_in).match(dateReg);
-    if (validate === -1) {
-        this.setAttribute('class', 'invalidInput');
-        alert('Your data is invalid');
-    }
-    else {
-        this.setAttribute('class', 'pass');
-        this.setAttribute('data-valid', 'valid');
-        submitEnable();
-    }
+function usernameValidation() {
+	var input = document.getElementById('username'),
+    	template = /^user_([a-zA-Z0-9@\.]*)/i,
+    	validate = template.test(input.value);
+	
+	checkInput(validate, input);
 }
 
 
-function nicknameValidation(nickname_in) {
-    // var val = nickname_in.value;
-    var nickReg = /^ [] $/;
-    var validate = String(nickname_in).match(nickReg);
-    if (validate === -1) {
-        this.setAttribute('class', 'invalidInput');
-        alert('Your data is invalid');
-    }
-    else {
-        this.setAttribute('class', 'pass');
-        this.setAttribute('data-valid', 'valid');
-        submitEnable();
-    }
+function dateValidation() {
+	var input = document.getElementById('joined'),
+    	dateReg = /([0-3][0-9])\/([0-1][0-9])\/([1-2][0-9][0-9][0-9])/,
+    	validate = dateReg.test(input.value);
+	
+    checkInput(validate, input);
+}
+
+
+function nicknameValidation() {
+	var input = document.getElementById('nickname'),
+		nickReg = /\w+\.\w+/,
+		validate = nickReg.test(input.value);
+	
+    checkInput(validate, input);
 }
 
 
@@ -179,5 +174,4 @@ function submitEnable() {
         submit.disabled = false;
     }
 }
-
 
